75dfcccb14c16d077edd27871a6462d4b9c69580
socratic-exe <lamichhanebhabis@gmail.com>
2024-10-22 15:13:01 -0600

    Added extension

--- Fact_Finder.py
+++ Fact_Finder.py
@@ -6,6 +6,11 @@ import pandas as pd # Referance: Geeksforgeeks
 # Initialize CSV file as a dataframe
 df = pd.read_csv("CIA Fact Finder.csv") # Referance: Geeksforgeeks
 
+# Print DataFrame without index Referance: Geeksforgeeks
+blankIndex=[''] * len(df)
+df.index=blankIndex
+
+
 def country():
     # Make it into a global variable for easiness
     global chosen_country
@@ -18,6 +23,7 @@ def country():
     new_country_list = []
     for element in country_list:
         new_country_list.append(element.replace(" ", ""))
+    print(new_country_list)
     # Ask the user for the country and change the user's input to prevent errors later
     chosen_country = input("Country? ").capitalize()
     chosen_country = chosen_country.replace(" ", "")
@@ -36,15 +42,20 @@ def country():
 
 
 
+def fact():
+    fact = metric()
+    metric_list = df[fact].tolist()
+    print("The", fact, "of", chosen_country, "is", metric_list[country_number])
+
 def metric():
     # Ask the user for the metric they would like
     chosen_metric = input(('''
-Metric? 
-1. Population
-2. Birth Rate
-3. GDP Per Capita
-4. Unemployment Rate
-    '''))
+    Metric? 
+    1. Population
+    2. Birth Rate
+    3. GDP Per Capita
+    4. Unemployment Rate
+        '''))
     # Change the numbers into
     if chosen_metric == "1":
         chosen_metric = "Population"
@@ -54,10 +65,22 @@ Metric?
         chosen_metric = "GDP Per Capita"
     elif chosen_metric == "4":
         chosen_metric = "Unemployment Rate"
-    metric_list = df[chosen_metric].tolist()
-    print("The", chosen_metric, "of", chosen_country, "is", metric_list[country_number])
+    return chosen_metric
+
+def sort():
+    sorting_metric = metric()
+    # Sorts the dataframe
+    ascending = input("Ascending? ")
+    if ascending == "YES":
+        sorted_df = df.sort_values(by=[sorting_metric], ascending=True) #Referance: Geeksforgeeks
+    else:
+        sorted_df = df.sort_values(by=[sorting_metric], ascending=False)
+    print(sorted_df[["Country", sorting_metric]])
+
+
 
 #def instructions():
 
 country()
-metric()
+fact()
+sort()
